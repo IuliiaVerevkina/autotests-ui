ddeac4da9c2b13ee9310ea49ee3207175dfa7691
Iuliia <110769038+IuliiaVerevkina@users.noreply.github.com>
2025-05-17 22:29:07 +0200

    7.3 Знакомство с PageComponent. Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,4 @@
 import pytest
-from playwright.sync_api import expect, Page
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
 
@@ -8,6 +7,8 @@ from pages.create_course_page import CreateCoursePage
 @pytest.mark.courses
 def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.navbar.check_visible("username")
     courses_list_page.check_visible_courses_title()
     courses_list_page.check_visible_empty_view()
     courses_list_page.check_visible_create_course_button()
