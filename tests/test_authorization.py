9ad5640ef784aeb497330cb25cd4d2df8f1d0684
Iuliia <110769038+IuliiaVerevkina@users.noreply.github.com>
2025-06-02 21:12:14 +0200

    7.4 Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,4 @@
 import pytest
-from playwright.sync_api import expect, Page
 from pages.login_page import LoginPage
 
 test_data = [
@@ -13,7 +12,8 @@ test_data = [
 @pytest.mark.parametrize("email, password", test_data)
 def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
     login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-    login_page.fill_login_form(email=email, password=password)
+    login_page.login_form.fill_login_form(email=email, password=password)
+    login_page.login_form.check_visible(email=email, password=password)
 
     # Нажимаем кнопку "Login"
     login_page.click_login_button()
--- tests/test_successful_registration.py
+++ tests/test_successful_registration.py
@@ -9,7 +9,8 @@ def test_fill_successful_registration_form(registration_page: RegistrationPage, 
     username = "testuser"
     password = "TestPassword123!"
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form(email=unique_email, username=username, password=password)
+    registration_page.registration_form.fill_registration_form(email=unique_email, username=username, password=password)
+    registration_page.registration_form.check_visible(email=unique_email, username=username, password=password)
     registration_page.click_registration_button()
 
-    dashboard_page.check_visible_dashboard_title()
+    dashboard_page.dashboard_component.check_visible()
